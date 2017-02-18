9f13ee098c73f74c398e0830b8e9cc8965dee5ac
ytnobody <ytnobody@gmail.com>
2017-02-18 16:31:24 +0900

    use time::piece

--- AddMessage/func.pl
+++ AddMessage/func.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 use utf8;
 use JSON::PP;
-use Data::Dumper;
+use Time::Piece;
 
 require '../azure_func.pl';
 
@@ -10,6 +10,9 @@ my $member = decode_json(read_from('inputDocument'));
 my $event  = decode_json(read_from('inputEvent'));
 
 $member->{message} ||= [];
-push @{$member->{message}}, $event->{message};
+push @{$member->{message}}, {
+    created_at => localtime->strftime('%Y-%m-%d %H:%M:%S'),
+    body       => $event->{message},
+};
 
 write_to(outputDocument => encode_json($member));
